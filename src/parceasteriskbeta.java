cc3d9d18166daabc2331085e111e9ebe8d2f5a70
Nikita Pisarenko <nikitapisarenko1995@gmail.com>
2022-11-20 14:57:15 +0300

    0.1 beta

--- src/parceasteriskbeta.java
+++ src/parceasteriskbeta.java
@@ -7,18 +7,36 @@ public class parceasteriskbeta {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
+        parceasterisk(s);
+    }
+
+
+    public static void parceasterisk(String s){
         while(s.contains("*")){
-            int end = s.indexOf("*");
+            int end = s.lastIndexOf("*");
             int begin = end-1;
-            if (s.charAt(end - 1) == ')' )){
+            if (s.charAt(begin) == ')' ){
                 int depth = - 1;
-                for (int i = end - 1; i >=0; i--){
+                for (int i = end - 2; i >=0; i--){
                     if (s.charAt(i) == ')') depth--;
                     if (s.charAt(i) == '(') depth ++;
-                    if (depth == 0) break;
+                    if (depth == 0) {
+                        begin = i;
+                        break;
+                    }
                 }
             }
-            String ans =
+            end +=1;
+            String ans = s.substring(begin, end);
+
+            if (ans.charAt(0) == '(') {
+               // System.out.println("dqwdqwdwdqdq");
+                ans = ans.substring(begin+1, ans.length()-2);
+            }
+            System.out.println("ans = " + ans);
+            parceasterisk(ans);
+            s = s.substring(0, begin) + s.substring(end);
+            System.out.println(" s = " + s);
         }
     }
 
